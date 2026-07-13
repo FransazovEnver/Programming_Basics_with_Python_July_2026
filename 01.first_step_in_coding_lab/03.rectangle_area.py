ac508312b1b0fb93c3432870bf1836f8e0dd46d9
Enver Fransazov <enverfransazov@gmail.com>
2026-07-13 08:39:02 +0300

    commit

---
+++ 01.first_step_in_coding_lab/03.rectangle_area.py
@@ -0,0 +1,6 @@
+side_a = int(input())
+side_b = int(input())
+
+sum = side_a * side_b
+
+print(sum)
---
+++ 01.first_step_in_coding_lab/07.projects_creation.py
@@ -0,0 +1,9 @@
+name_of_arhitect = input()
+numbers_of_projects = int(input())
+
+one_project = 3
+needed_hours = numbers_of_projects * one_project
+
+print(f'The architect {name_of_arhitect} will need {needed_hours} hours to complete {numbers_of_projects} project/s.')
+
+
