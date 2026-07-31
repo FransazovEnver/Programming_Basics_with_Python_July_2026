1a895d44f0b12ae0f3297e7df6bbdbb7187c4bb6
Enver Fransazov <enverfransazov@gmail.com>
2026-07-31 23:25:17 +0300

    feat: cinema tickets

--- 05_conditional_statements_advanced_lab/08_cinema_ticket.py
+++ 05_conditional_statements_advanced_lab/08_cinema_ticket.py
@@ -1,1 +1,19 @@
+day_of_week = input()
 
+tickets = ""
+
+if day_of_week == 'Monday':
+    tickets = 12
+elif day_of_week == 'Tuesday':
+    tickets = 12
+elif day_of_week == 'Wednesday':
+    tickets = 14
+elif day_of_week == 'Thursday':
+    tickets = 14
+elif day_of_week == 'Friday':
+    tickets = 12
+elif ((day_of_week == 'Saturday')
+    or (day_of_week == 'Sunday')):
+    tickets = 16
+
+print(tickets)
