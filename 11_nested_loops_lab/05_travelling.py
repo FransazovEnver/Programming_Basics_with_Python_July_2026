6093c64df1e97ebefbf21fbd376ab80ce2a46ebe
Enver Fransazov <enverfransazov@gmail.com>
2026-08-25 20:21:07 +0300

    feat: traveling

---
+++ 11_nested_loops_lab/05_travelling.py
@@ -0,0 +1,15 @@
+destinations = input()
+
+while destinations != 'End':
+    budget = float(input())
+    money = 0.0
+
+    while budget > money:
+        save_money = float(input())
+        money += save_money
+
+
+    print(f'Going to {destinations}!')
+
+    destinations = input()
+
