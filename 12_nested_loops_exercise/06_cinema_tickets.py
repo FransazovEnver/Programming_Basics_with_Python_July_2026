92bffa12666233e271283ae318362f7d80275dd4
Enver Fransazov <enverfransazov@gmail.com>
2026-08-28 08:46:07 +0300

    feat: cinema

---
+++ 12_nested_loops_exercise/06_cinema_tickets.py
@@ -0,0 +1,43 @@
+student_tickets = 0
+standard_tickets = 0
+kid_tickets = 0
+
+result = ""
+
+while True:
+    command = input()
+
+    if command == 'Finish':
+        break
+
+    capacity = int(input())
+    sold_tickets = 0
+
+    while sold_tickets < capacity:
+        ticket_type = input()
+
+        if ticket_type == 'End':
+            break
+
+        if ticket_type == 'student':
+            student_tickets += 1
+
+        elif ticket_type == 'standard':
+            standard_tickets += 1
+
+        elif ticket_type == 'kid':
+            kid_tickets += 1
+
+        sold_tickets += 1
+
+    result += f'{command} - {sold_tickets / capacity * 100:.2f}% full.\n'
+
+total_tickets = student_tickets + standard_tickets + kid_tickets
+
+result += (f'Total tickets: {total_tickets}\n'
+           f'{student_tickets / total_tickets * 100:.2f}% student tickets.\n'
+           f'{standard_tickets / total_tickets * 100:.2f}% standard tickets.\n'
+           f'{kid_tickets / total_tickets * 100:.2f}% kids tickets.')
+print(result)
+
+
